0563ad6c0556abcd9ee0da4b3832695272bddaa1
aludesk <aludesk@INACAP.CL>
2022-05-06 15:35:39 -0400

    Contacto

--- java.js
+++ java.js
@@ -55,19 +55,31 @@ let funcion = () => {
 }
 
 
+//Recibe datos de pantalla
 
 let entry = () => {
     let arreglo = []
+
+    //Forma Directa
 
     arreglo[0] = document.getElementById("input-1").value
     arreglo[1] = document.getElementById("input-2").value
     arreglo[2] = document.getElementById("input-3").value
     arreglo[3] = document.getElementById("input-4").value
 
+    //Forma Automatica
+
     for (let i = 0; i <4; i++){
-            arreglo[] = document.getElementById("p-1").value   
+            arreglo[i] = document.getElementById("input-"+(i+1)).value   
     }
 
+    //Escribiendo en pantalla
+
+    for (let j = 0; j < 4; j++){
+        window.setTimeout(function(){
+            document.getElementById("p-"+(j+1)).innerHTML =
+            arreglo[j]
+        }, 1000)
+    } console.log(arreglo)
     
-    document.getElementById("p-1").innerHTML = arreglo[0]
 }
